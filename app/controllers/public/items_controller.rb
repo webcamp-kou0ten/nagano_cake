fc08e304901264a755173319a2e317c150bcada6
kento89 <nerofuriku@gmail.com>
2021-07-16 08:47:25 +0000

    [Add]

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,2 +1,12 @@
 class Public::ItemsController < ApplicationController
+
+  def show
+    @item=Item.find(params[:id])
+  end
+
+  def index
+    @items=Item.all
+    @item = Item.page(params[:page]).per(10)
+  end
+
 end
