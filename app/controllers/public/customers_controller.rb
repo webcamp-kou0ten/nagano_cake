1f7919023ecf106d061311873506baa2bf2d14ca
EC2 Default User <ec2-user@ip-172-31-83-210.ec2.internal>
2021-07-17 05:29:29 +0000

    [add]カスタマーの詳細と編集画面

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,29 @@
 class Public::CustomersController < ApplicationController
+
+  def show
+    @customer = Customer.find(current_customer)
+  end
+
+  def edit
+    @customer = Customer.find(current_customer)
+  end
+
+  def update
+    @customer = Customer.find(current_customer)
+    @customer.update(customer_params)
+    redirect_to public_customers_path(current_customer)
+  end
+
+  def unsubscribe
+  end
+
+  def exit
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :phone_number, :email)
+  end
+
 end
