26d6db350da75d98ea86a615e0be7376d3f73297
nonishi <g.nonono24@gmail.com>
2021-07-17 19:55:41 +0000

    [Add]admin/items

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,21 +1,49 @@
 class Admin::ItemsController < ApplicationController
+# before_action :authenticate_admin!
   
   def new
+    @item = Item.new
+    @genres = Genre.all
   end
   
   def create
+    item = Item.new(item_params)
+    if item.save
+      redirect_to admin_items_path, notice: "You have created item successfully."
+    else
+      @item = item
+      @genres = Genre.all
+      render 'new'
+    end
   end
   
   def index
+    @items = Item.page(params[:page]).per(10)
   end
   
   def show
+    @item = Item.find(params[:id])
   end
   
   def edit
+    @item = Item.find(params[:id])
+    @genres = Genre.all
   end
   
   def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to admin_item_path(item), notice: "You have updated item successfully."
+    else
+      @genres = Genre.all
+      render "edit"
+    end
+  end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:genre_id, :name, :introduction, :price, :image, :sell_status)
   end
   
 end
