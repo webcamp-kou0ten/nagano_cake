204cab103e42445ee80e4b240f3782f7a35dcf87
nonishi <g.nonono24@gmail.com>
2021-07-19 03:11:31 +0000

    [Update]serch_genre

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -2,10 +2,12 @@ class Public::ItemsController < ApplicationController
 
   def show
     @item=Item.find(params[:id])
+    @genres = Genre.all
   end
 
   def index
     @items = Item.where(sell_status: false).page(params[:page]).per(8)
+    @genres = Genre.all
   end
 
   private
