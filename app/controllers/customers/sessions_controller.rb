e6d423d604adf1a8340a1c70c30c3bbc0f7a5279
EC2 Default User <ec2-user@ip-172-31-83-210.ec2.internal>
2021-07-19 05:13:51 +0000

    [add]customers/customer

--- app/controllers/customers/sessions_controller.rb
+++ app/controllers/customers/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Customers::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :reject_customer, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -24,4 +25,16 @@ class Customers::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  protected
+
+  def reject_customer
+    @customer = Customer.find_by(email: params[:customer][:email])
+    if @customer.customer_status == true
+      reset_session
+      flash[:notice] = "退会済みです"
+      redirect_to new_customer_session_path
+    end
+  end
+
 end
--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,23 +1,31 @@
 class Public::CustomersController < ApplicationController
+  before_action :authenticate_customer!
 
   def show
-    @customer = Customer.find(current_customer)
+    @customer = Customer.find(current_customer.id)
   end
 
   def edit
-    @customer = Customer.find(current_customer)
+    @customer = Customer.find(current_customer.id)
   end
 
   def update
-    @customer = Customer.find(current_customer)
+    @customer = Customer.find(current_customer.id)
     @customer.update(customer_params)
     redirect_to public_customers_path(current_customer)
   end
 
   def unsubscribe
+    @customer = Customer.find(current_customer.id)
   end
 
   def exit
+    @customer = Customer.find(current_customer.id)
+    @customer.customer_status = "true"
+    @customer.save
+    reset_session
+    flash[:notice] = "退会しました"
+    redirect_to root_path
   end
 
   private
