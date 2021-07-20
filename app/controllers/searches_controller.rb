fa921f03144e6d3b15c7a15eee1c435eefeba11d
nonishi <g.nonono24@gmail.com>
2021-07-20 07:18:06 +0000

    [Update]search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,14 +2,29 @@ class SearchesController < ApplicationController
   
   def search
     @value = params[:value]
-    @datas = search_for(@value).page(params[:page])
+    @how = params[:how]
+    @datas = search_for(@value, @how).page(params[:page])
     @genres = Genre.all
+    @genre = Genre.find(@value) if @how == "genre"
   end
   
   private
   
-  def search_for(value)
-    Item.where("name LIKE ?", "%#{value}%").or(Item.where(genre_id: value))
+  def item_search(value)
+    Item.where("name LIKE ?", "%#{value}%")
+  end
+  
+  def genre_seach(value)
+    Item.where(genre_id: value)
+  end
+  
+  def search_for(value, how)
+    case how
+    when "item"
+      item_search(value)
+    when "genre"
+      genre_seach(value)
+    end
   end
   
 end
