b68f3d7fcd700b2ecce861481a617d243f5eee7a
sagittarius3008 <83518855+sagittarius3008@users.noreply.github.com>
2021-07-20 19:37:16 +0900

    update cartmodel.rb
    numericalityを20へ修正

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,7 +2,7 @@ class CartItem < ApplicationRecord
   belongs_to :customer
   belongs_to :item
   
-  validates :amount, numericality: { in: 1..10 }
+  validates :amount, numericality: { in: 1..20 }
   
   def sum_of_price
     item.price * amount
