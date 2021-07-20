13ebfe335467485a609a27b4ef9cd96f274f9dd8
sagittarius3008 <83518855+sagittarius3008@users.noreply.github.com>
2021-07-20 16:48:54 +0900

    更新しました
    行間を修正

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -2,13 +2,13 @@ class Public::ItemsController < ApplicationController
 before_action :authenticate_customer!, except: [:index]
 
   def show
-    @item=Item.find(params[:id])
-    @cart_items=CartItem.new
+    @item = Item.find(params[:id])
+    @cart_items = CartItem.new
   end
 
   def create
-    @item=Item.find(parmas[:id])
-    @cart_item=@item.customer(item_id: item_id)
+    @item = Item.find(parmas[:id])
+    @cart_item = @item.customer(item_id: item_id)
     @cart_item.save
     redirect_to cart_items
   end
@@ -18,6 +18,7 @@ before_action :authenticate_customer!, except: [:index]
   end
 
   private
+  
   def items_params
     params.require(:item).permit(:image, :name, :introduction, :price, :sell_status)
   end
