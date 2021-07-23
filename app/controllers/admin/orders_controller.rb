03fd4dd06f028fe0b3c4d53caac977dcfa193b89
EC2 Default User <ec2-user@ip-172-31-83-210.ec2.internal>
2021-07-23 16:11:57 +0000

    オーダー一覧追加

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,16 @@
 class Admin::OrdersController < ApplicationController
+
+  def index
+    @orders = Order.all.page(params[:page]).per(10)
+  end
+
+  def show
+  end
+
+  def production_status
+  end
+
+  def order_status
+  end
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
-  enum payment_method: {0 => クレジットカード, 1 => 銀行振込}
-  enum order_status: {0 => 入会待ち, 1 => 入金確認, 2 => 製作中, 3 => 発送準備中, 4 => 発送済み}
+  enum payment_method: {0 => "クレジットカード", 1 => "銀行振込"}, _prefix: true
+  enum order_status: {0 => "入会待ち", 1 => "入金確認", 2 => "製作中", 3 => "発送準備中", 4 => "発送済み"}, _prefix: true
 
   has_many :order_items
   belongs_to :customer
