725c67c9d0202e5eafc2549fbab0042ec96dc6df
sagittarius3008 <sagittarius30008@gmail.com>
2021-07-25 15:16:11 +0000

    [Add]Japanese

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,7 @@
 class Admin::OrdersController < ApplicationController
 
   def index
-    @orders = Order.all.page(params[:page]).per(10)
+    @orders = Order.all.page(params[:page]).per(10).reverse_order
   end
 
   def show
