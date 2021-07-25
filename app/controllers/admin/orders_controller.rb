bc23d110893e7c52e4b1fbee7d22c9c589e1ffa0
EC2 Default User <ec2-user@ip-172-31-83-210.ec2.internal>
2021-07-25 08:13:09 +0000

    admin/orders

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -5,12 +5,45 @@ class Admin::OrdersController < ApplicationController
   end
 
   def show
+    @order = Order.find(params[:id])
+    @order_items = @order.order_items
+    @order_item = @order_items.pluck(:item_id)
+    @items = Item.find(@order_item)
+    @total = @order_items.inject(0) do |sum, item|
+      sum + item.total_of_price
+    end
   end
 
   def production_status
+    order = Order.find(params[:id])
+    order_items = order.order_items
+    order_item = order_items.find(params[:order_item][:id])
+    order_item.update(production_status: params[:order_item][:production_status].to_i)
+    if order_item.production_status == "製作中"
+      order.update( order_status: 2 )
+    elsif
+      count = 0
+      order_items.each do |order_item|
+        if order_item.production_status == "製作完了"
+          count += 1
+        end
+      end
+      if count == order_items.count
+        order.update( order_status: 3 )
+      end
+    end
+    redirect_to request.referer
   end
 
   def order_status
+    order = Order.find(params[:id])
+    order.update(order_status: params[:order][:order_status].to_i)
+      if order.order_status == "入金待ち"
+        order.order_items.update_all("'production_status' = 0")
+      elsif order.order_status == "入金確認"
+        order.order_items.update_all("'production_status' = 1")
+      end
+    redirect_to request.referer
   end
 
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,4 +2,9 @@ class OrderItem < ApplicationRecord
   enum production_status: { "着手不可":0, "製作待ち":1, "製作中":2, "製作完了":3 }
   belongs_to :order
   belongs_to :item
+
+  def total_of_price
+    item.price * amount * 1.1
+  end
+
 end
